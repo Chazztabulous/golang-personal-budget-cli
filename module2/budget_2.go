2722783ef4aa5be9b6c7c2be5abff4c8139efe5e
Nick Stricklett <nstricklett@costco.com>
2022-05-26 12:22:32 -0700

    adding newBudget to report

--- module2/budget_2.go
+++ module2/budget_2.go
@@ -75,6 +75,11 @@ func (b *Budget) RemoveItem(description string) {
 // CreateBudget creates a new budget with a specified max
 func CreateBudget(month time.Month, max float32) (*Budget, error) {
 	var newBudget *Budget
+	Budget := Budget{
+		Max: max,
+	}
+	newBudget = &Budget
+	report[month] = newBudget
 
 	return newBudget, nil
 }
